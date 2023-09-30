0fe3dd2594a5ba8fa4f6127d74816f77211ef40a
AlapanGhosh10 <alapanzico10@gmail.com>
2023-09-30 20:25:52 +0530

    selection sort added

--- src/sortingvisualizer/SelectionSort.java
+++ src/sortingvisualizer/SelectionSort.java
@@ -15,10 +15,10 @@ public class SelectionSort extends AbstractSort {
   private static final Color MININDX_COLOR = Color.ORANGE;
   private static final Color NEW_MININDX_COLOR = Color.LIGHTGREEN;
 
-  private ParallelTransition colorCNode(Bar[] arr, int a, int b, Color colorA, Color colorB) {
+  private ParallelTransition colorBar(Bar[] arr, int a, int b, Color colorA, Color colorB) {
     ParallelTransition pt = new ParallelTransition();
 
-    pt.getChildren().addAll(colorCNode(arr, colorA, a), colorCNode(arr, colorB, b));
+    pt.getChildren().addAll(colorBar(arr, colorA, a), colorBar(arr, colorB, b));
 
     return pt;
   }
@@ -30,19 +30,19 @@ public class SelectionSort extends AbstractSort {
 
     for (int i = 0; i < arr.length - 1; i++) {
       minIndx = i;
-      transitions.add(colorCNode(arr, NEW_MININDX_COLOR, minIndx));
+      transitions.add(colorBar(arr, NEW_MININDX_COLOR, minIndx));
 
       for (int j = i + 1; j < arr.length; j++) {
-        transitions.add(colorCNode(arr, SELECT_COLOR, j));
+        transitions.add(colorBar(arr, SELECT_COLOR, j));
         if (arr[j].getValue() < arr[minIndx].getValue()) {
           if (minIndx == i) {
-            transitions.add(colorCNode(arr, minIndx, j, MININDX_COLOR, NEW_MININDX_COLOR));
+            transitions.add(colorBar(arr, minIndx, j, MININDX_COLOR, NEW_MININDX_COLOR));
           } else {
-            transitions.add(colorCNode(arr, minIndx, j, Color.CRIMSON, NEW_MININDX_COLOR));
+            transitions.add(colorBar(arr, minIndx, j, Color.CRIMSON, NEW_MININDX_COLOR));
           }
           minIndx = j;
         } else {
-          transitions.add(colorCNode(arr, START_COLOR, j));
+          transitions.add(colorBar(arr, START_COLOR, j));
         }
       }
 
@@ -50,10 +50,10 @@ public class SelectionSort extends AbstractSort {
         transitions.add(swap(arr, i, minIndx));
       }
 
-        transitions.add(colorCNode(arr, START_COLOR, i, minIndx));
+        transitions.add(colorBar(arr, START_COLOR, i, minIndx));
     }
 
-    transitions.add(colorCNode(Arrays.asList(arr), SORTED_COLOR));
+    transitions.add(colorBar(Arrays.asList(arr), SORTED_COLOR));
 
     return transitions;
   }
